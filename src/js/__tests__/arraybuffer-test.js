be4a9bf427256cdcc046a00cec0e172810571355
Viktor <mr.viktor.tkachev@gmail.com>
2023-03-22 18:57:21 +0800

    added class ArrayBufferConverter and tests

--- src/js/__tests__/arraybuffer-test.js
+++ src/js/__tests__/arraybuffer-test.js
@@ -3,12 +3,6 @@ import getBuffer from '../getbuffer';
 
 const converter = new ArrayBufferConverter();
 
-test('should load an ArrayBuffer into the converter', () => {
-  const buffer = getBuffer();
-  converter.load(buffer);
-  expect(converter.buffer).toEqual(buffer);
-});
-
 test('should convert an ArrayBuffer to a string', () => {
   const expectedString = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
   const buffer = getBuffer();
